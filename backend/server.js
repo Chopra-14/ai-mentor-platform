f08177ffd53c9aed16a3058f7137548f10fff06d
teddy <sathvikakonakalla14@gmail.com>
2026-05-21 12:00:47 +0530

    fixed duplicate mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,16 @@
 const dotenv = require("dotenv");
 
 dotenv.config();
+
 const express = require("express");
-const router = express.Router();
-const mongoose = require("mongoose");
-
-const User = require("../models/User");
-const Quiz = require("../models/Quiz");
-
+const cors = require("cors");
 
 const connectDB = require("./config/db");
+
 const authRoutes = require("./routes/authRoutes");
 const quizRoutes = require("./routes/quizRoutes");
 const recommendationRoutes =
-require("./routes/recommendationRoutes");
-
+  require("./routes/recommendationRoutes");
 
 connectDB();
 
@@ -26,14 +22,16 @@ app.use(
     credentials: true
   })
 );
+
 app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/quiz", quizRoutes);
+
 app.use(
   "/api/recommendation",
   recommendationRoutes
 );
-
 
 app.get("/", (req, res) => {
   res.send("AI Learning Assistant Backend Running");
